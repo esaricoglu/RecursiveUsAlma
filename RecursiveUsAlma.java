8e20da529a42bed57078a48f7896e30a5fc6978f
esaricoglu <84877085+esaricoglu@users.noreply.github.com>
2023-02-03 14:38:28 +0300

    Add files via upload

---
+++ RecursiveUsAlma.java
@@ -0,0 +1,26 @@
+package Metotlar;
+
+import java.util.Scanner;
+
+public class RecursiveUsAlma {
+    static int power() {
+        int a, b, result = 1;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Taban değeri giriniz : ");
+        a = inp.nextInt();
+
+        System.out.print("Üs değerini giriniz : ");
+        b = inp.nextInt();
+
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+        System.out.println(result);
+        return power();
+    }
+
+    public static void main(String[] args) {
+        power();
+    }
+}
